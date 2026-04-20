559e58b78583158b9417f162a40a5cff78f8af14
fernando galan verez <fergalanverez@gmail.com>
2026-04-20 12:32:57 +0200

    Actualizacion

--- lab_exercise_04.py
+++ lab_exercise_04.py
@@ -18,7 +18,16 @@ def external_tts(text, filename="external_tts_output.mp3"):
     tts = gTTS(text=text, lang='en', slow=False)
     tts.save(filename)
     system = platform.system() 
-    os.system(f"start {filename}") 
+    os.system(f"start {filename}")
+# Load Local Audio File using SpeechRecognition
+def load_audio_file(file_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(file_path) as source:
+        print(f"Loading Local Audio File: {file_path}...]")
+        # Record the audio data from the file
+        audio_data = recognizer.record(source) 
+        print("Audio File Loaded Successfully")
+        return recognizer, audio_data
 # Execution Flow
 if __name__ == "__main__":
     print("SPEECH TASKS PIPELINE")
@@ -26,3 +35,6 @@ if __name__ == "__main__":
     local_tts("Hello. This is my local text to speech running offline.")
     # Test External TTS
     external_tts("And this is the external API text to speech running online.")
+    load_audio_file("C:\\Users\\ferga\\Documents\\Nueva carpeta\\NLP_portafolio_galan_fernando-1\\local_stt_input.wav")
+    
+    
