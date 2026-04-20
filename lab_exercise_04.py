61d255f02d422fc32a577ed50c67dc63e91fe834
fernando galan verez <fergalanverez@gmail.com>
2026-04-20 11:56:23 +0200

    Actualizcion

---
+++ lab_exercise_04.py
@@ -0,0 +1,27 @@
+import os
+import platform
+import pyttsx3
+import speech_recognition as sr
+from gtts import gTTS
+from textblob import TextBlob
+# Local Text to Speech (TTS) using pyttsx3
+def local_tts(text):
+    print("Executing Local TTS")
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 105)  # Set speech rate
+    engine.say(text)
+    engine.runAndWait()
+# External Text to Speech (TTS) using gTTS (Google Text-to-Speech API)
+def external_tts(text, filename="external_tts_output.mp3"):
+    print("Executing External API TTS Google Text-to-Speech")
+    tts = gTTS(text=text, lang='en', slow=False)
+    tts.save(filename)
+    system = platform.system() 
+    os.system(f"start {filename}") 
+# Execution Flow
+if __name__ == "__main__":
+    print("SPEECH TASKS PIPELINE")
+    # Test Local TTS
+    local_tts("Hello. This is my local text to speech running offline.")
+    # Test External TTS
+    external_tts("And this is the external API text to speech running online.")
